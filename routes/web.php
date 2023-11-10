418f1ad6d0034145f30afc46e598e5e5dc70356e
NimmimolAlp <nimmimol.alpts@gmail.com>
2023-11-10 07:20:46 +0530

    hai seeocnd

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/table', function () {
+    return view('table');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/register', function () {
+    return view('register');
+});
+Route::get('/create', function () {
+    return view('test');
+});
